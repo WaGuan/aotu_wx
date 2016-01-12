3c08c862fa0342896fdde1111dcfa1d1269f75f1
cnt1992 <418231398@qq.com>
2016-01-12 15:02:44 +0800

    update:搜索列表页

--- routes/list.js
+++ routes/list.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('superagent');
 
 var jssdk = require('../api/jssdk');
 var config = require('../config/config');
@@ -7,13 +8,27 @@ var appid = config.appid;
 
 // 获取文章列表
 router.get('/', function(req, res, next) {
-  
-  var url = req.query.url || 'http://weinre.qiang.it/aotu_wx/list';
 
-  new jssdk( url, function( data ){
-    console.log(data,'data');
-    res.render('page/list',{ data: data, appid: appid});  
-  });
+    var key = req.query.key;
+    var searchUrl = config.search_url;
+
+    request
+        .get( searchUrl + key )
+        .end( function(err, resp){
+            var result = JSON.parse(resp.text);
+            if( result.rtn === 0 ){
+                res.render('page/list',{ searchurl: config.search_url + key, result: result,data: {noncestr:'111',timestamp:'222',sinature:'333'}, appid: '1234567890'});                
+            }
+        });
+
+    
+  
+  // var url = req.query.url || 'http://weinre.qiang.it/aotu_wx/list';
+
+  // new jssdk( url, function( data ){
+  //   console.log(data,'data');
+  //   res.render('page/list',{ data: data, appid: appid});  
+  // });
   
 });
 
